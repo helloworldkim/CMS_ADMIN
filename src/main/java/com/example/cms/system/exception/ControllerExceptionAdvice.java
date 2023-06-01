74a0a2a9a634225478733fc7e4715ceeea134d1f
helloworldkim <arinwater@gmail.com>
2023-06-01 19:29:46 +0900

    [COMMIT] - 기본 Exception error 처리적용

---
+++ src/main/java/com/example/cms/system/exception/ControllerExceptionAdvice.java
@@ -0,0 +1,81 @@
+package com.example.cms.system.exception;
+
+import com.example.cms.system.constant.GlobalConst;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.TypeMismatchException;
+import org.slf4j.MDC;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import org.webjars.NotFoundException;
+
+import javax.naming.NoPermissionException;
+import javax.security.auth.message.AuthException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@ControllerAdvice
+public class ControllerExceptionAdvice {
+
+    /**
+     * 기본 Error처리.
+     *
+     * @param request
+     * @param response
+     * @param e
+     * @return
+     * @throws IOException
+     */
+    @ExceptionHandler(Exception.class)
+    public ModelAndView exceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception e)
+            throws IOException {
+
+        HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+
+        if (e instanceof IllegalArgumentException
+                || e instanceof IllegalStateException
+                || e instanceof TypeMismatchException
+                || e instanceof ServletRequestBindingException) {
+
+            httpStatus = HttpStatus.BAD_REQUEST;
+
+        } else if (e instanceof NotFoundException) {
+            httpStatus = HttpStatus.NOT_FOUND;
+
+        } else if (e instanceof AuthException) {
+            httpStatus = HttpStatus.UNAUTHORIZED;
+
+        } else if (e instanceof NoPermissionException) {
+            httpStatus = HttpStatus.FORBIDDEN;
+
+        }
+
+        Map<String, String> resultMap = getDefaultErrorResultMap(httpStatus, e.getMessage());
+        return new ModelAndView("error", resultMap);
+    }
+
+    /**
+     * Error 화면
+     *
+     * @param httpStatus
+     * @param exceptionMessage
+     * @return
+     */
+    private Map<String, String> getDefaultErrorResultMap(HttpStatus httpStatus, String exceptionMessage) {
+        Map<String, String> resultMap = new HashMap<>();
+        resultMap.put("status", Integer.toString(httpStatus.value()));
+        resultMap.put("error", exceptionMessage);
+        resultMap.put("timestamp", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+
+        log.info("==> sendError - httpStatus={}", httpStatus);
+        return resultMap;
+    }
+}
