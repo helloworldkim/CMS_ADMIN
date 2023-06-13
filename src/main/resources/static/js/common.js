c2462caea443e36c26c1c11e22acf929f3e3c514
helloworldkim <arinwater@gmail.com>
2023-06-13 11:09:15 +0900

    [COMMIT] - loginForm.html 아이디저장 추가 - 자바스크립트에 cookieUtil추가

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -5,3 +5,79 @@
 let fnLocationHref = (url) => {
     location.href = url;
 }
+
+let fnIsEmptyWithMessageAndFocus = (name, label) => {
+    const message = `${label} 값을 입력해 주세요.`;
+    if (fnIsEmpty(name)) {
+        alert(message);
+        fnMoveFocus(name);
+        return true;
+    }
+
+    return false;
+}
+
+let fnIsEmpty =  (name) => {
+    let isEmpty = true;
+    let targetElements = document.getElementsByName(name);
+
+    if (targetElements[0].type === "radio"
+        || targetElements[0].type === "checkbox") {
+        for (let targetElement of targetElements) {
+            if (targetElement.checked) {
+                isEmpty = false;
+                break;
+            }
+        }
+    } else {
+        isEmpty = targetElements[0].value.trim().length === 0;
+    }
+
+    return isEmpty;
+}
+
+let fnMoveFocus  = (name) => {
+    let targetElement = document.getElementsByName(name);
+    targetElement.focus();
+}
+
+const cookieUtil = {
+    fnGetCookie: function (name) {
+        let matches = document.cookie.match(new RegExp(
+            "(?:^|; )" + name.replace(/([.$?*|{}()\[\]\\\/+^])/g, "\\$1")
+            + "=([^;]*)"));
+        return matches ? decodeURIComponent(matches[1]) : undefined;
+    },
+
+    fnDeleteCookie: function (name) {
+        cookieUtil.fnSetCookie(name, "", {
+            "max-age": -1
+        })
+    },
+
+    fnSetCookie: function (name, value, options = {}) {
+
+        options = {
+            path: "/", // 필요한 경우, 호출 시 옵션 설정
+            ...options
+        };
+
+        if (options.expires instanceof Date) {
+            options.expires = options.expires.toUTCString();
+        }
+
+        let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(
+            value);
+
+        for (let optionKey in options) {
+            updatedCookie += "; " + optionKey;
+            let optionValue = options[optionKey];
+            if (optionValue !== true) {
+                updatedCookie += "=" + optionValue;
+            }
+        }
+
+        document.cookie = updatedCookie;
+    }
+
+};
