644ecae1db3a99aaa571c075222e158b8eb6ce34
helloworldkim <arinwater@gmail.com>
2023-07-26 15:42:01 +0900

    [TEST] - TEST코드 DisplayName 명확하게 정리 및 테스트 코드 정리

--- src/test/java/com/example/cms/system/interceptor/AuthInterceptorTest.java
+++ src/test/java/com/example/cms/system/interceptor/AuthInterceptorTest.java
@@ -3,6 +3,7 @@ package com.example.cms.system.interceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.regex.Matcher;
@@ -15,50 +16,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 class AuthInterceptorTest {
 
     @Test
+    @DisplayName("하나의 뎁스만 가진 메뉴로 접근시 뎁스 카운트는 1로 된다.")
     void oneDepthUrlTest() {
         String requestURI = "/login";
         String targetChar = "/";
-        Pattern pattern = Pattern.compile(Pattern.quote(targetChar));
-        Matcher matcher = pattern.matcher(requestURI);
-
-        int count = 0;
-        while (matcher.find()) {
-            count++;
-        }
+        int count = urlDepthCheck(requestURI, targetChar);
         log.info("count = {}", count);
         assertThat(count).isEqualTo(1);
         String requestURIPath = StringUtils.substring(requestURI, 0, requestURI.lastIndexOf("/") + 1);
     }
     @Test
+    @DisplayName("두개의 뎁스를 가진 메뉴로 접근시 뎁스 카운트는 2로 된다.")
     void twoDepthUrlTest() {
         String requestURI = "/system/menu";
         String targetChar = "/";
-        Pattern pattern = Pattern.compile(Pattern.quote(targetChar));
-        Matcher matcher = pattern.matcher(requestURI);
-
-        int count = 0;
-        while (matcher.find()) {
-            count++;
-        }
+        int count = urlDepthCheck(requestURI, targetChar);
         log.info("count = {}", count);
         assertThat(count).isEqualTo(2);
     }
     @Test
+    @DisplayName("두개의 이상의 뎁스를 가진 메뉴로 접근시 뎁스 카운트는 3이 된다.")
     void overTwoDepthUrlTest() {
         String requestURI = "/system/menu/1";
         String targetChar = "/";
-        Pattern pattern = Pattern.compile(Pattern.quote(targetChar));
-        Matcher matcher = pattern.matcher(requestURI);
-
-        int count = 0;
-        while (matcher.find()) {
-            count++;
-        }
+        int count = urlDepthCheck(requestURI, targetChar);
         log.info("count = {}", count);
         assertThat(count).isEqualTo(3);
     }
 
     @Test
+    @DisplayName("두개이상의 뎁스를 가진 메뉴로 접근시 requestURI는 2뎁스까지 끊어서 읽는다.")
     void requestURITest1() {
         //given
         String requestURI = "/system/menu/1";
@@ -71,6 +58,7 @@ class AuthInterceptorTest {
     }
 
     @Test
+    @DisplayName("두개의 뎁스를 가진 메뉴로 접근시 requestURI는 2뎁스까지 끊어서 읽는다.")
     void requestURITest2() {
         //when
         String requestURI = "/system/menu";
@@ -83,6 +71,7 @@ class AuthInterceptorTest {
     }
 
     @Test
+    @DisplayName("하나의 뎁스를 가진 메뉴로 접근시 requestURI는 1뎁스까지만 읽는다.")
     void requestURITest3() {
         //given
         String requestURI = "/login";
